812465af76d8f800ae95eedcbe5fa0df2837dec9
Anurag Garimella <anurag.garimella94@gmail.com>
2023-05-29 18:35:29 +0530

    Chapter 12.1 - Accept commandline args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // `args()` function will panic if there are any arguments containing invalid Unicode.
+    // `args_os()` produces `OsString` but that is platform DEPENDENT
+    let env_args: Vec<String> = env::args().collect();
+    
+    // Sample output
+    // dbg!(env_args);
+    // [src\main.rs:5] env_args = [
+    //     "target\\debug\\rust_io_project.exe",
+    //     "searchstring",
+    //     "hello.txt",
+    // ]
+
+    let query = &env_args[1];
+    let file_path = &env_args[2];
+
+    println!("Searching for : {}", query);
+    println!("In File : {}", file_path);
 }
